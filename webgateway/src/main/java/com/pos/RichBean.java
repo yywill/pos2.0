27d80b572a818d5c400dca89a263053e569e731d
William Yang <yywill@gmail.com>
2014-01-09 23:37:27 +0800

    解决了panelMenu传递参数的问题

--- webgateway/src/main/java/com/pos/RichBean.java
+++ webgateway/src/main/java/com/pos/RichBean.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 
 @ViewScoped
 @ManagedBean
@@ -13,6 +15,7 @@ public class RichBean implements Serializable {
     private static final long serialVersionUID = -6239437588285327644L;
 
     private String name;
+    private String current;
 
     @PostConstruct
     public void postContruct() {
@@ -26,4 +29,21 @@ public class RichBean implements Serializable {
     public void setName(String name) {
         this.name = name;
     }
+    
+    public String getCurrent(){
+    	return current;
+    }
+    
+    public void setCurrent(String current){
+    	this.current = current;
+    }
+    
+    public void updateCurrent(){
+    	FacesContext context = FacesContext.getCurrentInstance();
+        setCurrent((String) context.getExternalContext()
+             .getRequestParameterMap().get("current"));
+        
+        return ;
+    }
+    
 }
